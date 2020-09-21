76f404396e042682c96d4a3d94c89c09ae902620
iamkvv <vkondra@gmail.com>
2020-09-21 10:34:37 +0500

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -41,3 +41,7 @@ https://docs.google.com/presentation/d/e/2PACX-1vShT9L0JJLouWWMs3ZL8hAdu5vjtH4CP
 
         ============ COSCO ===================
 https://www.sysnettechsolutions.com/en/download-cisco-packet-tracer/
+    http://blog.netskills.ru/p/cisco-packet-tracer.html
+        
+        -----------------------
+http://www.mediafire.com/file/29cuudpyfh1s3it/cisco_packet_tracer_6.1.1_for_windows_%2528with_tutorials%2529_-_student_version.exe/file       
