f55e970f93d5ecc23dad73c956f7e9dce3d2c619
iamkvv <vkondra@gmail.com>
2020-10-13 10:58:00 +0500

    Update xml.py

--- xml.py
+++ xml.py
@@ -5,11 +5,20 @@ def getdata():
     tree = ET.parse('data.xml')
     root = tree.getroot()
 
+    find(root)
     show(root)
+
+def find(root):
+    for country in root.findall('country'):
+        rank = country.find('rank').text
+        name = country.get('name')
+        print(name, rank)
+
+    print('--------------------',end='\n\n')
 
 def show(x):
     for e in x:
-        print(e.attrib.get('name') if e.attrib else "" ,  e.text if e.text else "")
+        print(e.tag,e.attrib.get('name') if e.attrib else "" ,  e.text if e.text else "")
         show(e)
 
 
